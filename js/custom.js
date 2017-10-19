281d98b723dcb7dbe1c25fe0c80a7e1bc65df773
miqureshi <qureshi.mirfan@gmail.com>
2017-10-20 04:08:26 +0500

    client section slider added

--- js/custom.js
+++ js/custom.js
@@ -53,6 +53,26 @@ var buttons = $('.goDown, .goDownRed, .goDownGray');
     });
 
 
+var owl = $('.owl-carousel');
+		owl.owlCarousel({
+			margin: 10,
+			nav: true,
+			loop: true,
+			autoplay:true,
+    	autoplayTimeout:3000,
+			responsive: {
+				0: {
+					items: 1
+				},
+				600: {
+					items: 2
+				},
+				1000: {
+					items: 3
+				}
+			}
+		});
+
 });
 
 function getAutoHeight(){
